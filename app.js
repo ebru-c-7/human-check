5819ad3d6c727682e6df773c536cb604ad636de3
ebru-c-7 <ebru.cakmak00@hotmail.com>
2022-01-21 06:41:11 +0300

    cors package added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const { USER, checkRecaptcha } = require("./util.js");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/check-user", async (req, res) => {
   console.log(req.body);
