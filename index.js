35ec7a419b5c38f6e94f2942438155edeb4ef833
ebru-c-7 <ebru.cakmak00@hotmail.com>
2022-01-21 06:59:51 +0300

    file name change

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const path = require("path");
 const { USER, checkRecaptcha } = require("./util.js");
-const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use("/api/check-user", async (req, res) => {
   console.log(req.body);
@@ -43,11 +41,11 @@ app.use("/api/check-user", async (req, res) => {
   }
 });
 
-app.use(express.static("client/build")); //serving production assets
+app.use(express.static(path.join(__dirname, "./client/build"))); //serving production assets
 
 app.get("*", (req, res) => {
   //serving html file
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
